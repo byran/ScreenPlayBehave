a3c1ee33c00f9a8fe443ebbbb6a044fa366ca0e9
Byran Wills-Heath <byran@adgico.co.uk>
2021-09-06 23:30:16 +0100

    Add configuration option to json_formatter to allow output to a single file

--- screenplay_behave/json_formatter.py
+++ screenplay_behave/json_formatter.py
@@ -9,14 +9,30 @@ class JSONFormatter(CollectingFormatter):
 
         self.outputDirectory = path.join(config.base_dir, config.userdata['behave.formatter.spjson.path'])
 
+        if 'behave.formatter.spjson.single_file' in config.userdata:
+            self.singleFile = (config.userdata['behave.formatter.spjson.single_file'] == 'True')
+        else:
+            self.singleFile = False
+
+        self.features = []
+
     def eof(self):
         super().eof()
-        (_, file_name) = path.split(self.currentFeature.file_name)
-        (file_name, _) = path.splitext(file_name)
-        filePath = path.join(self.outputDirectory, file_name + '.json')
 
-        with open(filePath, 'wt') as file:
-            json.dump(self.currentFeature, file, default=lambda o: o.__dict__, indent=2)
+        if self.singleFile:
+            self.features.append(self.currentFeature)
+        else:
+            (_, file_name) = path.split(self.currentFeature.file_name)
+            (file_name, _) = path.splitext(file_name)
+            filePath = path.join(self.outputDirectory, file_name + '.json')
+
+            with open(filePath, 'wt') as file:
+                json.dump(self.currentFeature, file, default=lambda o: o.__dict__, indent=2)
 
     def close(self):
         super().close()
+
+        if self.singleFile:
+            filePath = path.join(self.outputDirectory, 'behave_test_results.json')
+            with open(filePath, 'wt') as file:
+                json.dump(self.features, file, default=lambda o: o.__dict__, indent=2)
